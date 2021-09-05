f9c9be5ced73bb5866c292ee7befdff1e9fafbc6
root <elogicocal@gmail.com>
2021-09-05 10:05:01 +0800

    init bilbili script

---
+++ BILIBILI-HELPER-PRE/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+version="1.0.3"
+
+function installJava(){
+  command -v apt >/dev/null 2>&1 && (apt-get update; apt-get install openjdk-8-jdk -y; return;)
+  command -v yum >/dev/null 2>&1 && (yum install java-1.8.0-openjdk -y; return;)
+}
+
+function installUnzip(){
+  command -v apt >/dev/null 2>&1 && (apt-get update; apt-get install unzip -y; return;)
+  command -v yum >/dev/null 2>&1 && (yum install unzip -y; return;)
+}
+
+command -v java >/dev/null 2>&1 || installJava
+java -jar ./BILIBILI-HELPER.jar $SESSDATA $DEDEUSERID $BILI_JCT $SCKEY >> message.txt
+cat message.txt
+echo "执行完成"
