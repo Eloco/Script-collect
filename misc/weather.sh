ea3e588f7680c8c397348f0a8be3a643ced4ae3d
root <elogicocal@gmail.com>
2021-09-06 10:41:55 +0800

    modify script

--- misc/weather.sh
+++ misc/weather.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl 'wttr.in/Shanghai?format=%l:+%c%20%t(feels:%20%f)%20%w%20%h%20%P%20%p%20%m\nSunrise:%09%S\nZenith:%09%z\nSunset:%09%s\nDusk:%09%d' -o $FILE_PATH
+curl 'wttr.in/Shanghai?format=%l:+%c%20%t(feels:%20%f)%20%w%20%h%20%P%20%p%20%m\nSunrise:%09%S\nZenith:%09%z\nSunset:%09%s\nDusk:%09%d&m&M' -o $FILE_PATH
