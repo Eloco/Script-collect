3a448114a12dd980714b252d41c62a9d8ec97bce
root <elogicocal@gmail.com>
2021-09-06 11:05:21 +0800

    modify script

--- misc/weather.sh
+++ misc/weather.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+LANGUAGE="zh-CN"
+UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
 
-curl 'wttr.in/Shanghai?format=%l:+%c(%C)%20%t(feels:%20%f)%20%w%20%h%20%P%20%p%20%m\nSunrise:%09%S\nZenith:%09%z\nSunset:%09%s\nDusk:%09%d&m&M' -o $FILE_PATH
+curl \
+  -H "Accept-Language: $LANGUAGE" \
+  -H "User-Agent: $UA" \
+  'wttr.in/Shanghai?format=%l:+%c(%C)%20%t(feels:%20%f)%20%w%20%h%20%P%20%p%20%m\nSunrise:%09%S\nZenith:%09%z\nSunset:%09%s\nDusk:%09%d&m&M' -o $FILE_PATH
