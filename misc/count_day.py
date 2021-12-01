bc4e03364e901f05a076ee0f7361b90876a464f1
root <elogicocal@gmail.com>
2021-12-01 21:23:04 +0800

    update count day

--- misc/count_day.py
+++ misc/count_day.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-from datetime import datetime, timedelta, timezone
+from datetime import datetime, timedelta, timezone, date
 
 tz_utc_8 = timezone(timedelta(hours=8))
 now = datetime.now(tz=tz_utc_8)
@@ -9,20 +9,13 @@ year  = int(now.strftime("%Y"))
 month = int(now.strftime("%m"))
 day   = int(now.strftime("%d"))
 
-days = 365
-months = [0,31,59,90,120,151,181,212,243,273,304]
+now=now.date()
 
-if 0 < month < 12:
-    sum = months[month - 1] + day
+next_year=date(int(year)+1,1,1)
+now_year =date(int(year),1,1)
 
-flag = 0
-if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-    flag = 1
-    days += 1
+sum = (now - now_year).days
 
-if flag == 1 and month > 2:
-    sum += 1
-
-surplus_day = days - sum
+surplus_day = (next_year - now).days
 
 print(f"今天是：{year}.{month}.{day}，是今年的第{sum}天，今年还剩{surplus_day}天")
